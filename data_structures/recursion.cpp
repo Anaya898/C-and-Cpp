855f8bcfd8ba73dfbbeece4f49dc41344560ecea
Anaya Shah <115178357+Anaya898@users.noreply.github.com>
2024-10-23 08:23:25 +0530

    Add files via upload

--- data_structures/recursion.cpp
+++ data_structures/recursion.cpp
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<iostream>
 
+using namespace std;
+// tail recursion
+void fun(int n){
+    if(n>0){
+        cout<<n<<endl;
+        fun(n-1);
+    }
+}
+// if function is called before cout, its head recursion;
+
+int main(){
+    int x=5;
+    fun(x);
+
+    return 0;
+}
